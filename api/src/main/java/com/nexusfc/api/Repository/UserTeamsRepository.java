902108c6e9919e9ebf05520b30175d0af5ec0df8
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-04-29 12:15:14 -0300

    feat: method to get user's team by user id

--- api/src/main/java/com/nexusfc/api/Repository/UserTeamsRepository.java
+++ api/src/main/java/com/nexusfc/api/Repository/UserTeamsRepository.java
@@ -1,9 +1,11 @@
 package com.nexusfc.api.Repository;
 
 import com.nexusfc.api.Model.UserTeam;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserTeamsRepository extends MongoRepository<UserTeam, String> {
+    UserTeam findByUserId(ObjectId id);
 }
